87b85dd901e4087a185cd2bb39e183052c814410
Ajay Kayyam <saiajay220@gmail.com>
2021-11-22 16:14:36 +0530

    nested route

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,5 +1,14 @@
+import { Route } from "react-router";
+
 const Welcome = () => {
-  return <h1>Welcome to My First Page</h1>;
+  return (
+    <section>
+      <h1>Welcome to My First Page</h1>
+      <Route path="/welcome/new-user">
+        <p>welcome, newuser!</p>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
